99532fe16addb59637d8727cc2b75dbd77de1b96
RobinMazars <45461136+RobinMazars@users.noreply.github.com>
2020-05-25 10:40:45 +0200

    first commit ex00 ex01

---
+++ ex01/exec.py
@@ -0,0 +1,8 @@
+import sys
+list = []
+for value in sys.argv[1:]:
+    value = ''.join(reversed(value))
+    list.append(value.swapcase())
+
+list = reversed(list)
+print(*list,end="")
---
+++ ex02/whois.py
@@ -0,0 +1,5 @@
+import sys
+if sys.argv[2] or type(sys.argv[1]) is int:
+    print("ERROR")
+else:
+    print("START")
