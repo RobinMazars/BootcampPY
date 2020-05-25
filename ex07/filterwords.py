5d1ee4cff6a3805a12a77fd30323100af0a4be0c
RobinMazars <45461136+RobinMazars@users.noreply.github.com>
2020-05-25 17:59:05 +0200

    ex07 ex08

---
+++ ex07/filterwords.py
@@ -0,0 +1,24 @@
+import sys
+import string
+if (len(sys.argv) == 3):
+    try:
+        s = sys.argv[1]
+        if s.isdigit() is True:
+            print("ERROR")
+        else:
+            n = int(sys.argv[2])
+            for i in string.punctuation:
+                s = s.replace(i, '')
+            list = s.split(" ")
+            print(list)
+            list2 = []
+            for w in list:
+                if len(w) > n:
+                    list2.append(w)
+            list = list2
+            print(list)
+
+    except ValueError:
+        print("ERROR")
+else:
+    print("ERROR")
